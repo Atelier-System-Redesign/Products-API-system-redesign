b05e9ae45c8ac5f7e3bdf2be1e231f57236f3e52
Michael O'Brien <mob61887@gmail.com>
2024-03-20 14:10:35 -0400

    completed controllers, removed _id and __v from model results

--- server/controller/getProducts.js
+++ server/controller/getProducts.js
@@ -1,3 +1,8 @@
 const { getProducts } = require('../model');
 
-module.exports = (req, res) => {};
+module.exports = (req, res) => {
+  const page = req.query.page || 1;
+  const count = req.query.count || 5;
+  getProducts(page, count)
+    .then((results) => res.send(results));
+};
--- server/controller/getStyles.js
+++ server/controller/getStyles.js
@@ -1,3 +1,16 @@
 const { getStyles } = require('../model');
 
-module.exports = (req, res) => {};
+module.exports = (req, res) => {
+  const productId = req.params.product_id;
+  getStyles(productId)
+    .then((results) => {
+      if (results.length) {
+        const result = { ...results[0] }._doc;
+        result.product_id = result.id.toString();
+        delete result.id;
+        res.send(result);
+      } else {
+        res.send([]);
+      }
+    });
+};
