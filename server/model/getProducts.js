7f5892aa97c6c38e9aca1c3b62f319f53c1e880c
Michael O'Brien <mob61887@gmail.com>
2024-03-22 02:30:01 -0400

    cleaned up model, everything works with API, time to test =D

--- server/model/getProducts.js
+++ server/model/getProducts.js
@@ -1,6 +1,18 @@
 const Product = require('../database/db');
 
-module.exports = (page, count) => Product.find({ id: 1 }, { _id: 0, __v: 0 })
+module.exports = (page, count) => Product.find({}, {
+  _id: 0,
+  __v: 0,
+  // id: 0,
+  // name: 0,
+  // slogan: 0,
+  // description: 0,
+  // category: 0,
+  // default_price: 0,
+  relatedItems: 0,
+  features: 0,
+  results: 0,
+})
   .sort({ id: 'asc' })
   .skip((page - 1) * count)
   .limit(count);
