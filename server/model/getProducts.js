e8309565680c5d8319d420a5e646da546d28be69
Michael O'Brien <mob61887@gmail.com>
2024-03-29 13:21:14 -0400

    improved getProducts

--- server/model/getProducts.js
+++ server/model/getProducts.js
@@ -1,18 +1,26 @@
 const Product = require('../database/db');
 
-module.exports = (page, count) => Product.find({}, {
-  _id: 0,
-  __v: 0,
-  // id: 0,
-  // name: 0,
-  // slogan: 0,
-  // description: 0,
-  // category: 0,
-  // default_price: 0,
-  relatedItems: 0,
-  features: 0,
-  results: 0,
-})
-  .sort({ id: 'asc' })
-  .skip((page - 1) * count)
-  .limit(count);
+module.exports = (page, count) => {
+  // Building the query condition based on whether `lastId` is provided
+  const lastId = page * count;
+  const queryCondition = {};
+  if (lastId) {
+    queryCondition.id = { $gt: lastId };
+  }
+
+  return Product.find(queryCondition, {
+    _id: 0,
+    __v: 0,
+    // id: 0,
+    // name: 0,
+    // slogan: 0,
+    // description: 0,
+    // category: 0,
+    // default_price: 0,
+    relatedItems: 0,
+    features: 0,
+    results: 0,
+  })
+    .sort({ id: 'asc' })
+    .limit(count);
+};
